b7bca9b3b05af10e4af369ec490ea83c27744cf0
Frank Koch <frankkoch@gmx.net>
2018-10-11 22:32:27 +0200

    Dev 2.2 Task 4

---
+++ classes/local/pages.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>;.
+/**
+ * Page utilities for multipage
+ *
+ * @package    mod_multipage
+ * @copyright  Richard Jones https://richardnz.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_multipage\local;
+defined('MOODLE_INTERNAL') || die();
+/**
+ * Utility class for counting pages and so on
+ *
+ * @package    mod_multipage
+ * @copyright  2018 Richard Jones https://richardnz.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class pages  {
+    /**
+     * Count the number of pages in a multipage mod
+     *
+     * @param int $multipageid the id of a simplelesson
+     * @return int the number of pages in the database that lesson has
+     */
+    public static function count_pages($multipageid) {
+        return 0;
+    }
+}
